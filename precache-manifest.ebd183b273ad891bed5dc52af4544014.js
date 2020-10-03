5e75228432ab0afc30dd49d45f1c322d787b73b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-03 17:11:34 +0000

    Deploy SArkar-Htet/bulb-on-off to github.com/SArkar-Htet/bulb-on-off.git:gh-pages

---
+++ precache-manifest.ebd183b273ad891bed5dc52af4544014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e924ef239331c706b11de5638658d7",
+    "url": "/bulb-on-off/index.html"
+  },
+  {
+    "revision": "301a135bd16fcd31ec3c",
+    "url": "/bulb-on-off/static/css/main.525383fc.chunk.css"
+  },
+  {
+    "revision": "2d9bf5ddf02ef9329c2b",
+    "url": "/bulb-on-off/static/js/2.179b107b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bulb-on-off/static/js/2.179b107b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301a135bd16fcd31ec3c",
+    "url": "/bulb-on-off/static/js/main.0f811b44.chunk.js"
+  },
+  {
+    "revision": "3c094268990cc6fd66a8",
+    "url": "/bulb-on-off/static/js/runtime-main.4025bb68.js"
+  }
+]);
