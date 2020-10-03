f6e628cea0d2f821c95a18d9156642b8b5467365
SArkarHtet <developer.soearkarhtet@gmail.com>
2020-10-03 23:36:53 +0630

    add pulbic path in img

--- src/Bulb.js
+++ src/Bulb.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 export default class Bulb extends Component {
   render() {
-    const bulbOff = '/pic_bulboff.gif';
-    const bulbOn = '/pic_bulbon.gif';
+    const bulbOff = process.env.PUBLIC_URL+'/pic_bulboff.gif';
+    const bulbOn = process.env.PUBLIC_URL+'/pic_bulbon.gif';
     const {isSwitchOn} = this.props;
     let imageURL = isSwitchOn ? bulbOn : bulbOff;
     return (
